d025a847c44b06c142ebdb423d83e657c6830afe
angelicaknudsen <eatingoreosrightnow@gmail.com>
2020-11-09 23:30:04 -0600

    ground shaking code

---
+++ radium.py
@@ -0,0 +1,8 @@
+print("wOW")
+
+userInput = input("Do you regularly eat room-temperature thrice-melted pumpkin-flavored candle wax? Enter 'y' for yes and 'n' or no.\nAnswer:")
+
+if userInput == "y":
+    print("Ah. I see that you are my kind of human.")
+else:
+    print("Who are you? The  a u d a c i t y")
